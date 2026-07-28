5388152c85dd84a05a66f925d61b6a36d3cd49c3
Виталий Шевченко <blackjack358@gmail.com>
2026-07-28 15:23:39 +0300

    Project: complete searching module

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,27 @@
-import {rules, createComparison} from "../lib/compare.js";
-
+import { rules, createComparison } from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+  // @todo: #5.1 — настроить компаратор
+  const compare = createComparison(
+    ["skipEmptyTargetValues"],
+    [
+      rules.searchMultipleFields(
+        searchField,
+        ["date", "customer", "seller"],
+        false,
+      ),
+    ],
+  );
 
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
+  return (data, state, action) => {
+    // @todo: #5.2 — применить компаратор
+    const searchValue = state[searchField] ?? "";
+    if (!searchValue) {
+      return { data, state };
     }
+
+    // Filter data through our own comparator
+    const filteredData = data.filter((row) => compare(row, state));
+    return { data: filteredData, state };
+  };
 }
